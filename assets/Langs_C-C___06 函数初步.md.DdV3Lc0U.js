c2f71dacff9f5c25deb511d911b8e62d3e55ff7b
ymz <935460098@qq.com>
2025-02-24 20:11:02 +0800

    🎉 init: 项目初始化，部署第一个版本

---
+++ assets/Langs_C-C___06 函数初步.md.DdV3Lc0U.js
@@ -0,0 +1,14 @@
+import{_ as a,c as i,o as n,ae as p}from"./chunks/framework.DI3LEz4j.js";const c=JSON.parse('{"title":"函数初步","description":"","frontmatter":{},"headers":[],"relativePath":"Langs/C-C++/06 函数初步.md","filePath":"Langs/C-C++/06 函数初步.md"}'),t={name:"Langs/C-C++/06 函数初步.md"};function l(e,s,h,k,E,r){return n(),i("div",null,s[0]||(s[0]=[p(`<h1 id="函数初步" tabindex="-1">函数初步 <a class="header-anchor" href="#函数初步" aria-label="Permalink to &quot;函数初步&quot;">​</a></h1><p>格式：</p><div class="language-c++ vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">c++</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">返回类型 函数名称(参数，参数，...)</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">{</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">	函数功能</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">	...</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">	</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">	return</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 返回值;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">// 使用 void 返回空类型</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">void</span><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;"> defname</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">(</span><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">args_</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">, ...)</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">{</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    函数功能</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    ...</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}</span></span></code></pre></div>`,3)]))}const o=a(t,[["render",l]]);export{c as __pageData,o as default};
